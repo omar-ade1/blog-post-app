c5d277d930fdeb803d33881468506e6a967b7777
omar adel <omar50001000@gmail.com>
2024-06-19 06:34:45 +0300

    cv

--- src/app/fetchApis/auth/loginUser.ts
+++ src/app/fetchApis/auth/loginUser.ts
@@ -3,7 +3,7 @@ import { DOMAIN_NAME } from "../../../utils/domain";
 
 export const LOGIN_USER = async (email: string, password: string) => {
   try {
-    const res = await axios.post(`${DOMAIN_NAME}/api/login`, {
+    const res = await axios.post(`api/login`, {
       email: email,
       password: password,
     });
